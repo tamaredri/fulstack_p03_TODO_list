0078b63bbc8a36e5d3959cc5b8b9d9c315d5c2e4
tamar edri <tedri@g.jct.ac.il>
2024-04-03 23:55:13 +0300

    finish implementing the server with status codes, the network and the FXMLHttpRequest.

--- index.js
+++ index.js
@@ -153,19 +153,44 @@ class DB {
 
 
 class Message {
-    constructor(method, url) {
-        this.method = method;
-        this.url = url;
-        this.content_type = "";
-        this.body_data = "";
+    set method(met){
+        this.request_method = met;
+    }
+    
+    get method(){
+        return this.request_method;
+    }
+
+    set url(url_string){
+        this.url_request = url_string;
+    }
+    
+    get url(){
+        return this.url_request;
     }
 
     set status_code(status) {
         this.status = status;
     }
 
+    get status_code(){
+        return this.status;
+    }
+
     set body(body) {
         this.body_data = body;
+    }
+
+    get body() {
+        return this.body_data;
+    }
+
+    set content_type(content){
+        this.content_type = content;
+    }
+
+    get content_type(){
+        return this.content_type;
     }
 }
 
@@ -179,7 +204,9 @@ class Server{
         // go the message fields - type Message
         var request = message.url.split('/');
         var body = JSON.parse(message.body);
-        var response = {};
+
+        var response = new Message();
+        response.status = "200 OK";
 
         switch (request[0]) {
             case 'users':
@@ -187,25 +214,43 @@ class Server{
                 switch (message.method) {
                     case 'GET':
                         // check credentials. if there was a false returned or wrong password - find an error with the request body.
-                        if (request.length != 2) return 404; // find the right error code for bad request.
+                        if (request.length != 2) {
+                            response.status = "400 Bad Request";
+                            break;
+                        } // the url was unrecognized
 
-                        var password = this.db_access.GET_users(request[1]);
+                        var user_data = this.db_access.GET_users(request[1]);
+                        
+                        if(!user_data){
+                            response.status = "404 Not Found"; // the user was not found
+                            break;
+                        }
 
-                        if(password === body.password) response = {'access': 'granted'};
-                        else response = {'access' : 'denied'};
+                        if(user_data.password != body.password) {
+                            response.status = "403 Forbidden"; // wrong password
+                            break;
+                        }
 
+                        // ?? response.body = user_data; // the request is done.
                         break;
                         
                     case 'POST':
                         // store the user in the DB and return a code according to the result of the DB function. 
-                        if (request.length != 1) return 404; // find the right error code for bad request.
+                        if (request.length != 1){
+                            response.status = "400 Bad Request";
+                            break;
+                        } // the url was unrecognized
 
-                        if(!this.db_access.POST_users(body.username, body.password)) return 404; // find the right error code for bad request.
+                        if(!this.db_access.POST_users(body.username, body.password)){
+                            response.status = "404 Not Found";
+                            break;
+                        } // the requested action could not be performed
 
                         break; 
         
                     default:
-                        // problem with the url - error code?
+                        response.status = "400 Bad Request";
+                        // the url was unrecognized
                         break;
                 }
                 break;
@@ -216,54 +261,75 @@ class Server{
                     case 'GET':
                         // check if the get requests the entire list or just 1 task. 
                         // according to the return value of the DB - decied the code to return
-                        if(2 > request.length > 3) return 404; // find the right error code for bad request.
+                        if(2 > request.length > 3){
+                            response.status = "400 Bad Request";
+                            break;
+                        } // the url was unrecognized
 
                         var taskid = -1;
                         if(request.length === 3) taskid = parseInt(request[2]);
 
                         var tasks = this.db_access.GET_tasks(request[1], taskid);
-                        if(!tasks) return 404; // find the right error code for bad request.
+                        if(!tasks) {
+                            response.status = "404 Not Found";
+                            break;
+                        } // the requested action could not be performed
 
-                        response = {'tasks' : tasks};
+                        response.body = {'tasks' : tasks};
 
                         break;
                         
                     case 'POST':
-                        if (request.length != 2) return 404; // find the right error code for bad request.
+                        if (request.length != 2) {
+                            response.status = "400 Bad Request";
+                            break;
+                        } // the url was unrecognized
                         
-                        if(!this.db_access.POST_tasks(request[1], body.description, body.note)) 
-                            return 404; // find the right error code for bad request.
+                        if(!this.db_access.POST_tasks(request[1], body.description, body.note))  {
+                            response.status = "404 Not Found";
+                            break;
+                        } // the requested action could not be performed
 
                         break; 
 
                     case 'PUT':
-                        if (request.length != 3) return 404; // find the right error code for bad request.
-                        
-                        if(!this.db_access.PUT_tasks(request[1], parseInt(request[2]), body.description, body.note, body.status)) 
-                            return 404; // find the right error code for bad request.
+                        if (request.length != 3) {
+                            response.status = "400 Bad Request";
+                            break;
+                        } // the url was unrecognized
+
+                        if(!this.db_access.PUT_tasks(request[1], parseInt(request[2]), body.description, body.note, body.status))  {
+                            response.status = "404 Not Found";
+                            break;
+                        } // the requested action could not be performed
 
                         break;
 
                     case 'DELETE':
-                        if (request.length != 3) return 404; // find the right error code for bad request.
+                        if (request.length != 3) {
+                            response.status = "400 Bad Request";
+                            break;
+                        } // the url was unrecognized
                         
-                        if(!this.db_access.DELETE_tasks(request[1], parseInt(request[2]))) 
-                            return 404; // find the right error code for bad request.
+                        if(!this.db_access.DELETE_tasks(request[1], parseInt(request[2])))  {
+                            response.status = "404 Not Found";
+                            break;
+                        } // the requested action could not be performed
 
                         break;
 
                     default:
-                        // invalid request - return an error message
+                        response.status = "400 Bad Request";
                         break;
                 }
                 break;
         
             default:
-                // problem with the url - error code?
+                response.status = "400 Bad Request";
                 break;
         }
         
-        // no problem accured during the request processing
+        // send the response to the caller
         parseDoneCallBack(response);
     }    
 }
@@ -273,14 +339,22 @@ class Network{
     constructor(){
         this.server = get_server();
     }
-    request(message, func){
+
+    request(message, call_back){
         // randomly send 500 ERROR code - the server is not responding
-        this.response_function = func;
-        this.server.parseMessage(message, this.response);
+        if(Math.floor(Math.random() * 3) === 0){
+            var response_messag = new Message();
+            response_messag.status = "503 Service Unavailable";
+
+            call_back(response_messag);
+        } else{
+            this.response_call_back = call_back;
+            this.server.parseMessage(message, this.response);
+        }
     }
 
     response(message){
-        this.response_function(message);
+        this.response_call_back(message);
     }
 
 }
@@ -289,26 +363,30 @@ class Network{
 class FXMLHttpRequest{
     constructor(){
         this.network = get_network();
+        this.readyState = 0; // 0: request not initialized
     }
 
-    open(method, url, async){
-        this. request_message = new Message(method, url);
-
+    open(method, url){ // specifies the request
+        this.request_message = new Message();
+        this.request_message.url = url;
+        this.request_message.method = method;
     }
 
-    send(body = ""){
+    send(body = {}){ // sends the request
         this.request_message.body = body;
         this.network.request(this.request_message, receive_response);
+        this.readyState = 3; //3: processing request
     }
     
-    receive_response(message){
-        this.response = message;
-        this.onload_func();
-
+    receive_response(message){ // callback function for the network to return its response.
+        this.readyState = 4; //4: request finished and response is ready
+        this.response_message = message;
+        this.status = message.status;
+        this.onload_func(); // define default function?
     }
 
-    set onload(func){
-        this.onload_func = func;
+    set onload(callback){ // a callback function the FXMLHttpRequest will call once the response is received
+        this.onload_func = callback;
     }
 }
 
